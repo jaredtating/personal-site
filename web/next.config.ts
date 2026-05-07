b0787a641ad6c91cc58ddf1d0f4bf841c629477e
jaredtating <jaredtating@berkeley.edu>
2026-05-06 22:53:49 -0700

    Updated assetPrefix

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: "/personal-site",
   basePath: "/personal-site",
   images: {
     unoptimized: true,
