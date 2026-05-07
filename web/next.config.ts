f0fc7f73988f3057960ff66de6a92442359aa5d8
jaredtating <jaredtating@berkeley.edu>
2026-05-06 22:36:00 -0700

    Additional github page configs

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/personal-site"
+  basePath: "/personal-site",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
