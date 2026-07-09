ec9f25c80ec5c22d5540f9ef40044f63601036e2
jaredtating <jaredtating@berkeley.edu>
2026-07-08 22:55:30 -0700

    Added space below buttons

--- web/app/_components/Header.tsx
+++ web/app/_components/Header.tsx
@@ -60,7 +60,7 @@ const Header = () => {
       </div>
 
       {/* Links section */}
-      <div className="flex flex-col gap-8 text-center lg:text-left lg:flex-row items-center content-main justify-end z-10 row-start-2">
+      <div className="flex flex-col gap-8 text-center lg:text-left lg:flex-row items-center content-main justify-end z-10 row-start-2 pb-10">
         <div className="gap-2 flex flex-col items-center lg:items-start">
           {sections.map(({ text, id }) => (
             <GlitchText
