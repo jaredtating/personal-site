7e9de8a7b79e030a1c30f5c697e6f0ff248a5d4e
jaredtating <jaredtating@berkeley.edu>
2026-07-08 22:27:33 -0700

    Fixed hamburger visibility

--- web/app/_components/Nav.tsx
+++ web/app/_components/Nav.tsx
@@ -42,6 +42,7 @@ const Nav = () => {
           <PiLogo />
         </Link>
         <RxHamburgerMenu
+          className="md:hidden"
           onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
           size={60}
         />
